c195cfaa1938e2f206fc36872f8b851d95df620d
mezz <tehgeek@gmail.com>
2017-01-22 22:21:25 -0800

    Fix concurrent modification exceptions from networking

--- src/main/java/binnie/core/network/BinniePacketHandler.java
+++ src/main/java/binnie/core/network/BinniePacketHandler.java
@@ -2,9 +2,13 @@ package binnie.core.network;
 
 import binnie.core.AbstractMod;
 import binnie.core.network.packet.MessageBinnie;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.IThreadListener;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
 
 public abstract class BinniePacketHandler implements IMessageHandler<MessageBinnie, IMessage> {
 	private IPacketProvider provider;
@@ -19,17 +23,32 @@ public abstract class BinniePacketHandler implements IMessageHandler<MessageBinn
 
 	@Override
 	public IMessage onMessage(final MessageBinnie message, final MessageContext ctx) {
-		try {
-			final int packetId = message.id;
-			for (final IPacketID id : this.provider.getPacketIDs()) {
-				if (id.ordinal() == packetId) {
-					id.onMessage(message, ctx);
-					return null;
+		final int packetId = message.id;
+		for (final IPacketID id : this.provider.getPacketIDs()) {
+			if (id.ordinal() == packetId) {
+				if (ctx.side == Side.CLIENT) {
+					onClientMessage(id, message, ctx);
+				} else {
+					onServerMessage(id, message, ctx);
 				}
+				return null;
 			}
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
 		}
 		return null;
+	}
+
+	private static void onClientMessage(final IPacketID id, final MessageBinnie message, final MessageContext ctx) {
+		checkThreadAndEnqueue(id, message, ctx, Minecraft.getMinecraft());
+	}
+
+	private static void onServerMessage(final IPacketID id, final MessageBinnie message, final MessageContext ctx) {
+		EntityPlayerMP player = ctx.getServerHandler().playerEntity;
+		checkThreadAndEnqueue(id, message, ctx, player.getServerWorld());
+	}
+
+	private static void checkThreadAndEnqueue(final IPacketID id, final MessageBinnie message, final MessageContext ctx, IThreadListener threadListener) {
+		if (!threadListener.isCallingFromMinecraftThread()) {
+			threadListener.addScheduledTask(() -> id.onMessage(message, ctx));
+		}
 	}
 }
