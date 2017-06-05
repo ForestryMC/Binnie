9fb03893b266006f84120e29cf3d876993713a34
marcin212 <marcin212@protonmail.com>
2017-06-05 23:50:28 +0200

    Fixed wrong import

--- src/main/java/binnie/genetics/machine/LaboratoryMachine.java
+++ src/main/java/binnie/genetics/machine/LaboratoryMachine.java
@@ -13,7 +13,6 @@ import binnie.genetics.machine.acclimatiser.PackageAcclimatiser;
 import binnie.genetics.machine.analyser.PackageAnalyser;
 import binnie.genetics.machine.genepool.PackageGenepool;
 import binnie.genetics.machine.incubator.PackageIncubator;
-import com.sun.istack.internal.NotNull;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -66,7 +65,6 @@ public enum LaboratoryMachine implements IMachineType {
 	}
 
 	public static class ComponentGUIHolder extends MachineComponent implements INetwork.TilePacketSync, IInteraction.RightClick {
-		@NotNull
 		private ItemStack stack = ItemStack.EMPTY;
 
 		public ComponentGUIHolder(final IMachine machine) {
