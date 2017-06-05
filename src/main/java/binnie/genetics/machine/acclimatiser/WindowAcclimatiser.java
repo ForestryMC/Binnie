dde90817d7608bf83bd55151e217f8fdee079a28
marcin212 <marcin212@protonmail.com>
2017-06-05 20:44:43 +0200

    Fixed progress texture for Acclimatiser

--- src/main/java/binnie/genetics/machine/acclimatiser/WindowAcclimatiser.java
+++ src/main/java/binnie/genetics/machine/acclimatiser/WindowAcclimatiser.java
@@ -6,12 +6,14 @@ import binnie.core.craftgui.geometry.Position;
 import binnie.core.craftgui.minecraft.control.ControlEnergyBar;
 import binnie.core.craftgui.minecraft.control.ControlErrorState;
 import binnie.core.craftgui.minecraft.control.ControlPlayerInventory;
+import binnie.core.craftgui.minecraft.control.ControlProgress;
 import binnie.core.craftgui.minecraft.control.ControlSlot;
 import binnie.core.craftgui.minecraft.control.ControlSlotArray;
 import binnie.core.craftgui.resource.Texture;
 import binnie.core.craftgui.resource.minecraft.StandardTexture;
 import binnie.genetics.Genetics;
 import binnie.genetics.core.GeneticsTexture;
+import binnie.genetics.machine.analyser.WindowAnalyser;
 import binnie.genetics.machine.craftgui.WindowMachine;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
@@ -19,8 +21,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class WindowAcclimatiser extends WindowMachine {
-	static Texture ProgressBase = new StandardTexture(64, 0, 130, 21, GeneticsTexture.GUIProcess3);
-	static Texture Progress = new StandardTexture(64, 21, 130, 21, GeneticsTexture.GUIProcess3);
+	static Texture ProgressBase = new StandardTexture(0, 64, 64, 64, GeneticsTexture.GUIProcess3);
+	static Texture Progress = new StandardTexture(0, 64, 64, 64, GeneticsTexture.GUIProcess3);
 
 	public WindowAcclimatiser(final EntityPlayer player, final IInventory inventory, final Side side) {
 		super(280, 198, player, inventory, side);
@@ -30,6 +32,7 @@ public class WindowAcclimatiser extends WindowMachine {
 	@SideOnly(Side.CLIENT)
 	public void initialiseClient() {
 		super.initialiseClient();
+		new ControlProgress(this, 65, 28 , WindowAcclimatiser.ProgressBase, WindowAcclimatiser.Progress, Position.RIGHT);
 		int x = 16;
 		final int y = 32;
 		new ControlSlotArray.Builder(this, x, y, 2, 2).create(Acclimatiser.SLOT_RESERVE);
