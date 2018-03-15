c945f1a73708bf4e3dddb10f3ab60eeb7e6988f1
Русских Анатолий <A.Russkih@dvf.rosmorport.ru>
2018-03-15 14:19:23 +1000

    Register ItemHoneyDrop research suitability for BeeManager, TreeManager and ButterflyManager

--- extrabees/src/main/java/binnie/extrabees/items/ItemHoneyDrop.java
+++ extrabees/src/main/java/binnie/extrabees/items/ItemHoneyDrop.java
@@ -4,8 +4,12 @@ import net.minecraft.item.ItemStack;
 
 import forestry.api.core.Tabs;
 import forestry.core.items.IColoredItem;
+import forestry.api.apiculture.BeeManager;
+import forestry.api.arboriculture.TreeManager;
+import forestry.api.lepidopterology.ButterflyManager;
 
 import binnie.extrabees.items.types.EnumHoneyDrop;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class ItemHoneyDrop extends ItemProduct<EnumHoneyDrop> implements IColoredItem {
 
@@ -14,6 +18,9 @@ public class ItemHoneyDrop extends ItemProduct<EnumHoneyDrop> implements IColore
 		this.setCreativeTab(Tabs.tabApiculture);
 		this.setUnlocalizedName("honey_drop");
 		setRegistryName("honey_drop");
+		BeeManager.beeRoot.setResearchSuitability(new ItemStack(this, 1, OreDictionary.WILDCARD_VALUE), 0.5f);
+		TreeManager.treeRoot.setResearchSuitability(new ItemStack(this, 1, OreDictionary.WILDCARD_VALUE), 0.5f);
+		ButterflyManager.butterflyRoot.setResearchSuitability(new ItemStack(this, 1, OreDictionary.WILDCARD_VALUE), 0.5f);
 	}
 
 	@Override
