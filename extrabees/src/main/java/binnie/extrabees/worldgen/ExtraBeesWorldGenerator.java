480bb50fdef4517752fd59f8e0217a4a2e48b9aa
temp1011 <temp1011github@gmail.com>
2018-03-28 12:28:17 +0100

    Use API, don't register if hives not enabled

--- extrabees/src/main/java/binnie/extrabees/worldgen/ExtraBeesWorldGenerator.java
+++ extrabees/src/main/java/binnie/extrabees/worldgen/ExtraBeesWorldGenerator.java
@@ -1,14 +1,20 @@
 package binnie.extrabees.worldgen;
 
-import forestry.apiculture.ModuleApiculture;
+import forestry.api.apiculture.hives.HiveManager;
+
+import binnie.core.util.Log;
 
 public class ExtraBeesWorldGenerator {
 
 	public void doInit() {
-		ModuleApiculture.getHiveRegistry().registerHive("aqua", BinnieHiveDescription.WATER);
-		ModuleApiculture.getHiveRegistry().registerHive("marbla", BinnieHiveDescription.MARBLE);
-		ModuleApiculture.getHiveRegistry().registerHive("saxum", BinnieHiveDescription.ROCK);
-		ModuleApiculture.getHiveRegistry().registerHive("basalt", BinnieHiveDescription.NETHER);
+		if (HiveManager.hiveRegistry == null) {
+			Log.warning("Can't get hive registry, no hives will be generated in the world");
+			return;
+		}
+		HiveManager.hiveRegistry.registerHive("aqua", BinnieHiveDescription.WATER);
+		HiveManager.hiveRegistry.registerHive("marbla", BinnieHiveDescription.MARBLE);
+		HiveManager.hiveRegistry.registerHive("saxum", BinnieHiveDescription.ROCK);
+		HiveManager.hiveRegistry.registerHive("basalt", BinnieHiveDescription.NETHER);
 
 	}
 }
