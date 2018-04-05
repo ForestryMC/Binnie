221511a73deba49848447056e12f2a9c9d27ead6
RusTit <titanofdarkness@yandex.ru>
2018-04-05 18:39:16 +1000

    Refactor WorldGenHiveWater.java

--- extrabees/src/main/java/binnie/extrabees/worldgen/WorldGenHiveWater.java
+++ extrabees/src/main/java/binnie/extrabees/worldgen/WorldGenHiveWater.java
@@ -2,8 +2,8 @@ package binnie.extrabees.worldgen;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -20,12 +20,15 @@ public class WorldGenHiveWater extends WorldGenHive {
 
 	@Override
 	public boolean generate(final World world, final Random random, final BlockPos blockPos) {
-		final IBlockState blockState = world.getBlockState(blockPos);
-		if (blockState.getBlock() != Blocks.WATER && blockState.getBlock() != Blocks.FLOWING_WATER) {
+		final Block block = world.getBlockState(blockPos).getBlock();
+		if (block != Blocks.WATER && block != Blocks.FLOWING_WATER) {
 			return false;
 		}
 		final Material materialBlockBelow = world.getBlockState(blockPos.down()).getMaterial();
-		if (materialBlockBelow == Material.SAND || materialBlockBelow == Material.CLAY || materialBlockBelow == Material.GROUND || materialBlockBelow == Material.ROCK) {
+		if (materialBlockBelow == Material.SAND ||
+				materialBlockBelow == Material.CLAY ||
+				materialBlockBelow == Material.GROUND ||
+				materialBlockBelow == Material.ROCK) {
 			world.setBlockState(blockPos, ExtraBees.hive.getDefaultState().withProperty(BlockExtraBeeHives.HIVE_TYPE, EnumHiveType.WATER));
 		}
 		return true;
