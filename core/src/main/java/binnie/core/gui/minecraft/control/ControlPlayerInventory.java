c72e4c1d9aee6b3a2bb768587dda167c2d529c0f
RusTit <titanofdarkness@yandex.ru>
2018-04-07 21:12:10 +1000

    Small refactoring in ControlPlayerInventory.java

--- core/src/main/java/binnie/core/gui/minecraft/control/ControlPlayerInventory.java
+++ core/src/main/java/binnie/core/gui/minecraft/control/ControlPlayerInventory.java
@@ -15,20 +15,24 @@ public class ControlPlayerInventory extends Control {
 
 	public ControlPlayerInventory(final IWidget parent, final boolean wide) {
 		super(parent, parent.getSize().xPos() / 2 - (wide ? 110 : 81), parent.getSize().yPos() - (wide ? 54 : 76) - 12, wide ? 220 : 162, wide ? 54 : 76);
-		this.slots = new ArrayList<>();
-		for (int row = 0; row < 3; ++row) {
-			for (int column = 0; column < 9; ++column) {
-				final ControlSlot slot = new ControlSlot.Builder(this, (wide ? 58 : 0) + column * 18, row * 18).assign(InventoryType.PLAYER, 9 + column + row * 9);
+		final int firstRowColumn = 9;
+		final int rowCount = 3;
+		final int columnCount = 9;
+		this.slots = new ArrayList<>(firstRowColumn + rowCount*columnCount);
+		for (int row = 0; row < rowCount; ++row) {
+			for (int column = 0; column < columnCount; ++column) {
+				final ControlSlot slot = new ControlSlot.Builder(this, (wide ? 58 : 0) + column * 18, row * 18)
+                        .assign(InventoryType.PLAYER, firstRowColumn + column + row * columnCount);
 				this.slots.add(slot);
 			}
 		}
 		if (wide) {
-			for (int i1 = 0; i1 < 9; ++i1) {
+			for (int i1 = 0; i1 < firstRowColumn; ++i1) {
 				final ControlSlot slot2 = new ControlSlot.Builder(this, i1 % 3 * 18, i1 / 3 * 18).assign(InventoryType.PLAYER, i1);
 				this.slots.add(slot2);
 			}
 		} else {
-			for (int i1 = 0; i1 < 9; ++i1) {
+			for (int i1 = 0; i1 < firstRowColumn; ++i1) {
 				final ControlSlot slot2 = new ControlSlot.Builder(this, i1 * 18, 58).assign(InventoryType.PLAYER, i1);
 				this.slots.add(slot2);
 			}
@@ -41,10 +45,13 @@ public class ControlPlayerInventory extends Control {
 
 	public ControlPlayerInventory(final IWidget parent, final int x, final int y) {
 		super(parent, x, y, 54, 220);
+		final int rowCount = 6;
+		final int columnCount = 6;
 		this.slots = new ArrayList<>();
-		for (int row = 0; row < 6; ++row) {
-			for (int column = 0; column < 6; ++column) {
-				final ControlSlot slot = new ControlSlot.Builder(this, column * 18, row * 18).assign(InventoryType.PLAYER,column + row * 6);
+		for (int row = 0; row < rowCount; ++row) {
+			for (int column = 0; column < columnCount; ++column) {
+				final ControlSlot slot = new ControlSlot.Builder(this, column * 18, row * 18)
+                        .assign(InventoryType.PLAYER,column + row * columnCount);
 				this.slots.add(slot);
 			}
 		}
