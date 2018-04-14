6db93dcbe79bd1d47740a90e78fb954442c65c59
RusTit <titanofdarkness@yandex.ru>
2018-04-14 11:44:57 +1000

    Improve variable.

--- core/src/main/java/binnie/core/gui/controls/button/ControlButton.java
+++ core/src/main/java/binnie/core/gui/controls/button/ControlButton.java
@@ -57,7 +57,7 @@ public class ControlButton extends Control {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void onRenderBackground(int guiWidth, int guiHeight) {
-		Object texture;
+		final Object texture;
 		if (this.isMouseOver()) {
 			texture = CraftGUITexture.BUTTON_HIGHLIGHTED;
 		} else if (this.isEnabled()) {
