5c1b7808c6068438cfa50f177c62ef5f1aa2bd8d
temp1011 <temp1011github@gmail.com>
2018-06-20 16:48:44 +0100

    add comments to lang file

--- genetics/src/main/java/binnie/genetics/config/ConfigurationMain.java
+++ genetics/src/main/java/binnie/genetics/config/ConfigurationMain.java
@@ -3,6 +3,7 @@ package binnie.genetics.config;
 import net.minecraftforge.common.config.Configuration;
 
 import binnie.core.config.ConfigFile;
+import binnie.core.util.I18N;
 
 @ConfigFile(filename = "/config/forestry/genetics/main.conf")
 public class ConfigurationMain implements IConfigurable {
@@ -38,29 +39,31 @@ public class ConfigurationMain implements IConfigurable {
 	public void configure(Configuration config) {
 		config.addCustomCategoryComment(MACHINES, "Energy and time settings for machines");
 
-		acclimatiserEnergy = config.getInt("acclimatiserEnergy", MACHINES, acclimatiserEnergy, 0, 10, "Energy used per tick by the acclimatiser");
+		acclimatiserEnergy = config.getInt("acclimatiserEnergy", MACHINES, acclimatiserEnergy, 0, 10, I18N.localise("genetics.config.acclimatiser.energy"));
 
-		analyserEnergy = config.getInt("analyserEnergy", MACHINES, analyserEnergy, 0, 50000, "Energy used per process by the analyzer");
-		analyserTime = config.getInt("analyserTime", MACHINES, analyserTime, 0, 1500, "Time per process for the analyzer");
+		analyserEnergy = config.getInt("analyserEnergy", MACHINES, analyserEnergy, 0, 50000, I18N.localise("genetics.config.analyser.energy"));
+		analyserTime = config.getInt("analyserTime", MACHINES, analyserTime, 0, 1500, I18N.localise("genetics.config.analyser.time"));
 
-		genepoolEnergy = config.getInt("genepoolEnergy", MACHINES, genepoolEnergy, 0, 50000, "Energy used per process by the genepool");
-		genepoolTime = config.getInt("genepoolTime", MACHINES, genepoolTime, 0, 1500, "Time per process for the genepool");
+		genepoolEnergy = config.getInt("genepoolEnergy", MACHINES, genepoolEnergy, 0, 50000, I18N.localise("genetics.config.genepool.energy"));
+		genepoolTime = config.getInt("genepoolTime", MACHINES, genepoolTime, 0, 1500, I18N.localise("genetics.config.genepool.time"));
 
-		incubatorEnergy = config.getInt("incubatorEnergy", MACHINES, incubatorEnergy, 0, 10, "Energy used by the incubator per tick");
+		incubatorEnergy = config.getInt("incubatorEnergy", MACHINES, incubatorEnergy, 0, 10, I18N.localise("genetics.config.incubator.energy"));
 
-		inoculatorEnergy = config.getInt("inoculatorEnergy", MACHINES, inoculatorEnergy, 0, 100000, "Energy used per gene by the inoculator");
-		inoculatorTime = config.getInt("inoculatorTime", MACHINES, inoculatorTime, 0, 20000, "Time per gene for the inoculator");
+		inoculatorEnergy = config.getInt("inoculatorEnergy", MACHINES, inoculatorEnergy, 0, 100000, I18N.localise("genetics.config.inoculator.energy"));
+		inoculatorTime = config.getInt("inoculatorTime", MACHINES, inoculatorTime, 0, 20000, I18N.localise("genetics.config.inoculator.time"));
 
-		isolatorEnergy = config.getInt("isolatorEnergy", MACHINES, isolatorEnergy, 0, 400000, "Energy used per process by the isolator");
-		isolatorTime = config.getInt("isolatorTime", MACHINES, isolatorTime, 0, 10000, "Time per process for the isolator");
+		isolatorEnergy = config.getInt("isolatorEnergy", MACHINES, isolatorEnergy, 0, 400000, I18N.localise("genetics.config.isolator.energy"));
+		isolatorTime = config.getInt("isolatorTime", MACHINES, isolatorTime, 0, 10000, I18N.localise("genetics.config.isolator.time"));
 
-		polymeriserEnergy = config.getInt("polymeriserEnergy", MACHINES, polymeriserEnergy, 0, 200000, "Energy used per process by the polymeriser");
-		polymeriserTime = config.getInt("polymeriserTime", MACHINES, polymeriserTime, 0, 5000, "Time per process by the polymeriser");
+		polymeriserEnergy = config.getInt("polymeriserEnergy", MACHINES, polymeriserEnergy, 0, 200000, I18N.localise("genetics.config.polymeriser.energy"));
+		polymeriserTime = config.getInt("polymeriserTime", MACHINES, polymeriserTime, 0, 5000, I18N.localise("genetics.config.polymeriser.time"));
 
-		sequencerTimeMultiplier = config.getInt("sequencerTimeMultiplier", MACHINES, sequencerTimeMultiplier, 0, 40000, "Time multiplier for sequencer");
-		sequencerEnergyMultiplier = config.getInt("sequencerEnergyMultiplier", MACHINES, sequencerEnergyMultiplier, 0, 40, "Energy multiplier for sequencer");
+		sequencerEnergyMultiplier = config.getInt("sequencerEnergyMultiplier", MACHINES, sequencerEnergyMultiplier, 0, 40, I18N.localise("genetics.config.sequencer.energy"));
+		sequencerTimeMultiplier = config.getInt("sequencerTimeMultiplier", MACHINES, sequencerTimeMultiplier, 0, 40000, I18N.localise("genetics.config.sequencer.time"));
 
-		splicerEnergy = config.getInt("splicerEnergy", MACHINES, splicerEnergy, 0, 30000000, "Base energy for the splicer");
-		splicerTime = config.getInt("splicerTime", MACHINES, splicerTime, 0, 500, "Base time for the splicer");
+		splicerEnergy = config.getInt("splicerEnergy", MACHINES, splicerEnergy, 0, 30000000, I18N.localise("genetics.config.splicer.energy"));
+		splicerTime = config.getInt("splicerTime", MACHINES, splicerTime, 0, 500, I18N.localise("genetics.config.splicer.time"));
+
 	}
+
 }
